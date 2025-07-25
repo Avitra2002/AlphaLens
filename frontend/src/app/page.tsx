a691005257be5ce50d32aca4fe99f135254bf8ce
Phon Avitra <phonavitra@Phons-MacBook-Pro-2.local>
2025-07-25 13:21:39 +0700

    Config frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-10 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600 underline">
+        Hello, Tailwind in Next.js!
+      </h1>
+    </main>
+  );
+}
